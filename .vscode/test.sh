f456adc6780d3d70c543058e376bc27857c8502b
南山竹 <junjiemars@gmail.com>
2018-01-05 14:07:13 +0800

    format

--- .vscode/test.sh
+++ .vscode/test.sh
@@ -6,7 +6,8 @@ if ! [[ $PS1 =~ ^\(python_koans\)$ ]]; then
   source ${workspaceRoot}/bin/activate
 fi
 
-major_version="`python -V 2>&1|sed -e 's/^Python\ \([0-9]*\)\.[0-9]*\.[0-9]*/\1/'`"
+major_version="`python -V 2>&1 | \
+  sed -e 's/^Python\ \([0-9]*\)\.[0-9]*\.[0-9]*/\1/'`"
 
 cd ${workspaceRoot}/python${major_version}
 python contemplate_koans.py
