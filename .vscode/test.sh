4003992141016c9a23b1b1857a36230e768ba41f
南山竹 <junjiemars@gmail.com>
2018-01-07 00:38:05 +0800

    major_version via python call

--- .vscode/test.sh
+++ .vscode/test.sh
@@ -6,8 +6,9 @@ if ! [[ $PS1 =~ ^\(python_koans\)$ ]]; then
   source ${workspaceRoot}/bin/activate
 fi
 
-major_version="`python -V 2>&1 | \
-  sed -e 's/^Python\ \([0-9]*\)\.[0-9]*\.[0-9]*/\1/'`"
+#major_version="`python -V 2>&1 | \
+#  sed -e 's/^Python\ \([0-9]*\)\.[0-9]*\.[0-9]*/\1/'`"
+major_version="`python -c 'import sys; print(sys.version_info[0])'`"
 
 cd ${workspaceRoot}/python${major_version}
 python contemplate_koans.py
